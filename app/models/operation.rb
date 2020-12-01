5625872b78d2d791a7eafe10dc9949e71768692a
SWillames <sergiowoc@gmail.com>
2020-12-01 06:41:46 -0300

    Creating test models (Operation).

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,15 @@
 class Operation < ApplicationRecord
   belongs_to :account
+  KIND = %w[whithdraw deposit transfer]
+  belongs_to :account
+  validates_presence_of :account, :amount
+  validates :kind, presence: true, inclusion: { in: KIND }
+  validate :check_withdraw
+  validates :amount, numericality: { greater_than: 0 }
+
+  private
+
+  def check_withdraw
+    errors.add(:amount, 'Not enough balance in account!') if kind == "withdraw" && amount > account.balance
+  end 
 end
--- spec/models/operation_spec.rb
+++ spec/models/operation_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Operation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "associations" do
+    it { is_expected.to belong_to(:account) } 
+  end
+
+  context "validations" do
+    it { is_expected.to validate_presence_of(:amount) }
+    it { is_expected.to validate_presence_of(:kind) }
+    it { is_expected.to validate_numericality_of(:amount).is_greater_than(0) }
+  end
 end
