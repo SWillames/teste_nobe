f4b5350eb4f7413505be3b5e74af6ce0f58d5d88
SWillames <sergiowoc@gmail.com>
2020-12-01 03:22:04 -0300

    Visitor opens the site.

---
+++ spec/features/visitor_open_home_page_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'Visitor open home page' do
+  scenario 'successfully' do
+    
+    visit root_path
+
+
+    expect(page).to have_link('Sign_in')
+    expect(page).to have_link('Sign_up')
+    expect(page).to have_content('Welcome Visitor')
+    expect(page).to have_link('Deposit')
+    expect(page).to have_link('Withdraw')
+  end
+end
