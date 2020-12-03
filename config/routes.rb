ceea49bd13c3f81eefe8e5c7f9de692107d985b5
SWillames <sergiowoc@gmail.com>
2020-12-03 03:35:26 -0300

    Defining the link of the operations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root "pages#home"
   devise_for :customers, :controllers => { registrations: 'registrations' }
+  get 'operations/new_deposit', to: 'operations#new_deposit', as: "deposit"
+  post 'operations/new_deposit', to: 'operations#create_deposit'
+  get 'operations/new_withdraw', to: 'operations#new_withdraw', as: "withdraw"
+  post 'operations/new_withdraw', to: 'operations#create_withdraw'
+  resources :operations, only: [:show] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
